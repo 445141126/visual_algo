683c962f9fb41a664a8e036b6f8ca09f65d31b6c
whbb <445141126@qq.com>
2016-02-29 22:06:27 +0800

    select sort

--- sort/js/sort.js
+++ sort/js/sort.js
@@ -1,18 +1,60 @@
 var bars = function(nums, h) {
     this.arr = [];
+    this.selected = -1;
+    this.mini = -1;
     this.paintEvent = function(c) {
         var rect = new ctx.rect(0, 0, 0, 0);
-        for(var i=0;i<this.arr.length;i++) {
-            rect.x = i*10;
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.selected == i) {
+                rect.color = 'rgba(0,0,0,0.5)'
+            }
+            if (this.mini == i) {
+                rect.color = 'red'
+            }
+            if (i != this.mini && i != this.selected) {
+                rect.color = 'black';
+            }
+            rect.x = i * 10;
             rect.y = 0;
-            rect.h = (this.arr[i]+1) * h;
+            rect.h = (this.arr[i] + 1) * h;
             rect.w = 10;
             rect.paintEvent(c);
         }
     }
 
-    for(var i=0;i<nums;i++) {
+    for (var i = 0; i < nums; i++) {
         this.arr.push(i);
+    }
+
+    this.sort = function() {
+        var self = this;
+        var _sort = function(i) {
+            if (i >= self.arr.length) {
+                self.selected = -1;
+                self.mini = -1;
+                return;
+            }
+            self.mini = i;
+
+            var inner = function(j) {
+                if (j >= self.arr.length) {
+                    swap(self.arr, i, self.mini);
+                    setTimeout(function() {
+                        _sort(i + 1);
+                    }, 200);
+                    return;
+                }
+                self.selected = j;
+                setTimeout(function() {
+                    if (self.arr[j] < self.arr[self.mini]) {
+                        self.mini = j;
+                    }
+                    inner(j + 1);
+                }, 100)
+            }
+            inner(i);
+        }
+        _sort(0);
     }
 }
 
@@ -36,36 +78,17 @@ function swap(arr, x, y) {
     arr[y] = tmp;
 };
 
-function select_sort(array, cmp) {
-    var _sort = function(i) {
-        if(i >= array.length) {
-            return;
-        }
-        var index = i;
-        for(var j = i; j<array.length; j++) {
-            if(cmp(array[j], array[index])) {
-                index = j;
-            }
-        }
-        swap(array, index, i, function(){
-            _sort(i+1);
-        });
-    };
-    
-    _sort(0);
-}
-
 function shuffle(array) {
-    for(var _ in array) {   
+    for (var _ in array) {
         var i = Math.floor(Math.random() * array.length);
         var j = Math.floor(Math.random() * array.length);
         swap(array, i, j);
     }
 }
 
+
+// main
 ctx.init('canvas');
-
-
 var b = new bars(50, 4);
 ctx.add_obj(b);
 
@@ -76,7 +99,5 @@ random_btn.addEventListener('click', function() {
 
 var sort_btn = document.getElementById('sort');
 sort_btn.addEventListener('click', function() {
-    select_sort(array, function(a, b) {
-        return a.y - b.y;
-    })
+    b.sort();
 })
